a96c92cec01db0d2754a5e78ed48904641f34816
Ksawery Brzeski <ksawerybrzeski99@gmail.com>
2021-09-14 13:52:33 +0200

    Fix existing tests

--- src/routine/routine.service.spec.ts
+++ src/routine/routine.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoutineService } from './routine.service';
+import { PrismaService } from '../prisma.service';
+import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
+import { RoutineController } from './routine.controller';
 
 describe('RoutineService', () => {
   let service: RoutineService;
 
+  //todo: mock prisma
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoutineService],
+      imports: [AuthModule, UserModule],
+      controllers: [RoutineController],
+      providers: [RoutineService, PrismaService],
     }).compile();
 
     service = module.get<RoutineService>(RoutineService);
